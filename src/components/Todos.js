0a487f0864595fb7a950bd7cd888b06ffcdc5d33
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-11 15:15:10 -0500

    init

---
+++ src/components/Todos.js
@@ -0,0 +1,38 @@
+import React,{useState} from 'react';
+import '../../src/App.css';
+
+
+
+const Todos = () => {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue]= useState('');
+    
+
+    console.log(todos);
+
+
+    return (
+        <div class="bigbox">
+                <h1>My TO DO LIST</h1>
+            <div className="inputbar input-group mb-3">
+                <input type="text" className="form-control" placeholder="Add new todo" aria-label="Add new todo" name={inputValue} value={inputValue} onChange={e => setInputValue(e.target.value)} aria-describedby="button-addon2"></input>
+                <div className="input-group-append">
+                    <button className="btn btn-success" type="button" onClick={() => {
+                        setTodos(prevTodos => [...prevTodos, inputValue])
+                        setInputValue("")
+                    }
+                    }>Add</button>
+                </div>
+            </div>
+            <ul >
+                {todos.map(todo => (
+                    <li key = {todo.ind}>
+                        {todo}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Todos;
